b90a2d965d762db93f2346ebbe10886009ce797d
Attaneva21 <attaneva21@codingburgas.bg>
2025-07-02 04:56:11 +0300

    Create mostly working index page

---
+++ WEB/src/app/components/authentication/register/register.component.ts
@@ -0,0 +1,51 @@
+﻿import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+  successMessage = '';
+  errorMessage = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      phoneNumber: [''],
+      address: [''],
+      city: [''],
+      dateOfBirth: ['', Validators.required],
+      education: [''],
+      workExperience: [''],
+      role: ['Candidate', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: () => {
+          this.successMessage = 'Registration successful!';
+          this.errorMessage = '';
+          this.registerForm.reset({ role: 'Candidate' });
+        },
+        error: () => {
+          this.errorMessage = 'Registration failed.';
+          this.successMessage = '';
+        }
+      });
+    }
+  }
+}
