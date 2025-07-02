23e74afe128df3a70f689f4b9df44c496e8e4747
Attaneva21 <attaneva21@codingburgas.bg>
2025-07-02 05:21:58 +0300

    Fix landing page style

--- WEB/src/app/app.routes.ts
+++ WEB/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RegisterComponent } from './components/authentication/register/register.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent }
 ];
